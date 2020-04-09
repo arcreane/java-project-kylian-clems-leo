b9d9f578fa3ebe017c96ab6f23494928113afdeb
Kylian <kylian.petitgenet@hotmail.fr>
2020-04-09 16:48:40 +0200

    player file

--- src/game/personnage/Player.java
+++ src/game/personnage/Player.java
@@ -5,30 +5,6 @@ import game.environnement.Shield;
 
 public class Player extends Character {
 	public int NbLifes;
-	public int getNbLifes() {
-		return NbLifes;
-	}
-
-	public void setNbLifes(int nbLifes) {
-		NbLifes = nbLifes;
-	}
-
-	public Shield getShield() {
-		return Shield;
-	}
-
-	public void setShield(Shield shield) {
-		Shield = shield;
-	}
-
-	public Laser getLaser() {
-		return Laser;
-	}
-
-	public void setLaser(Laser laser) {
-		Laser = laser;
-	}
-
 	public Shield Shield;
 	public Laser Laser;
 	public void Shoot() {
