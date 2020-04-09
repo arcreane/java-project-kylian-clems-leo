97e9b66f8187ebe5fd68b6dc547688190e46f943
Kylian <kylian.petitgenet@hotmail.fr>
2020-04-09 14:02:04 +0200

    add map and display the map

--- src/game/Program.java
+++ src/game/Program.java
@@ -3,10 +3,14 @@ package game;
 import game.terrain.Map;
 
 public class Program {
-	public Map map;
+	public static Map map;
 
 	public static void main(String[] args) {
 		var test = 1;
 		System.out.println(test);
+		
+		map = new Map();
+		
+		map.draw();
 	}
 }
