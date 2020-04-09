8b9c6d8f1c2e525f95d34196e60aadfa49403fcc
leonard <leo151098@hotmail.fr>
2020-04-09 16:29:44 +0200

    push life

--- src/game/environnement/Life.java
+++ src/game/environnement/Life.java
@@ -3,7 +3,18 @@ package game.environnement;
 import game.personnage.Player;
 
 public class Life extends Bonus {
+	int NbLifeToAdd;
+	
+	public Life(int nbLifeToAdd) {
+		super();
+		NbLifeToAdd = nbLifeToAdd;
+	}
+	
 	public void ApplyEffects(Player kevin) {
-		kevin.addNbLifes();
+	
+		kevin.addNbLifes(NbLifeToAdd);
+		
+		
+		
 	}
 }
